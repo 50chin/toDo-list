b53a731249ec0f3089ce4bbace2e0b3c71158ed8
50chin <50chin50@gmail.com>
2024-03-29 14:09:05 +0300

    finished toDo basic

--- src/script/app.js
+++ src/script/app.js
@@ -1,24 +1,25 @@
-const formNode = document.querySelector("form");
-const inputNode = document.querySelector(".toDo__input");
-const btnAddNode = document.querySelector(".toDo__btn-add");
-const listsNode = document.querySelector(".toDo__lists");
-const toDoBottomNode = document.querySelector(".toDo__bottom");
-const buttonsNode = document.querySelector(".toDo__buttons");
-const btnDelNode = document.querySelector(".toDo__btn-deleteFin");
-const btnDelAllNode = document.querySelector(".toDo__btn-deleteAll");
+const formNode = document.querySelector('form');
+const inputNode = document.querySelector('.toDo__input');
+const btnAddNode = document.querySelector('.toDo__btn-add');
+const listsNode = document.querySelector('.toDo__lists');
+const toDoBottomNode = document.querySelector('.toDo__bottom');
+const buttonsNode = document.querySelector('.toDo__buttons');
+const btnDelNode = document.querySelector('.toDo__btn-deleteFin');
+const btnDelAllNode = document.querySelector('.toDo__btn-deleteAll');
+const toDoAddListsNode = document.querySelector('.toDo_add-lists');
 
-let toDo = [];
+let toDo = JSON.parse(localStorage.getItem('toDo-lists')) ?? [];
 
-btnDelAllNode.addEventListener("click", btnDelAllHandler);
+btnDelAllNode.addEventListener('click', btnDelAllHandler);
 
 function btnDelAllHandler() {
   toDo = [];
   renderToDo(toDo);
-  listsNode.style.display = "none";
-  toDoBottomNode.style.display = "none";
+  toDoAddListsNode.style.display = 'none';
+  toDoBottomNode.style.display = 'none';
 }
-btnAddNode.addEventListener("click", inputHandler);
-formNode.addEventListener("submit", inputHandler);
+btnAddNode.addEventListener('click', inputHandler);
+formNode.addEventListener('submit', inputHandler);
 
 function inputHandler(evt) {
   evt.preventDefault();
@@ -31,25 +32,25 @@ function inputHandler(evt) {
   } else {
     return;
   }
-  inputNode.value = "";
+  inputNode.value = '';
   renderToDo(toDo);
   listsNode.scrollTop = -listsNode.scrollHeight;
 }
 //функция на скрытие кнопок
 function hiddenBtn() {
   if (!toDo.length) {
-    listsNode.style.display = "none";
-    toDoBottomNode.style.display = "none";
+    toDoAddListsNode.style.display = 'none';
+    toDoBottomNode.style.display = 'none';
   } else {
-    listsNode.style.display = "block";
-    toDoBottomNode.style.display = "block";
+    toDoAddListsNode.style.display = 'block';
+    toDoBottomNode.style.display = 'block';
   }
 }
-// Функция рендериться ТуДушка
+// Функция рендерится ТуДушка
 function createToDo({ id, text, done }) {
   // Метод по массиву ТуДу
-  const list = document.createElement("li");
-  list.className = "toDo__list";
+  const list = document.createElement('li');
+  list.className = 'toDo__list';
   list.innerHTML = `
     <span class="toDo__left">
     <input class="toDo__check" type="checkbox">
@@ -58,35 +59,36 @@ function createToDo({ id, text, done }) {
     <button class="toDo__btn-cross">❌</button>
     `;
 
-  const btnDeleteCrossNode = list.querySelector(".toDo__btn-cross");
-  btnDeleteCrossNode.addEventListener("click", () => {
+  const btnDeleteCrossNode = list.querySelector('.toDo__btn-cross');
+  btnDeleteCrossNode.addEventListener('click', () => {
     btnDeleteCrossHandler(id);
   });
 
-  const checkboxNode = list.querySelector(".toDo__check");
+  const checkboxNode = list.querySelector('.toDo__check');
   checkboxNode.checked = done;
-  checkboxNode.addEventListener("change", () => {
+  checkboxNode.addEventListener('change', () => {
     checkboxChecked(id);
   });
   return list;
 }
 
 function renderToDo(toDo) {
-  listsNode.innerHTML = "";
+  listsNode.innerHTML = '';
   toDo.forEach((el) => {
     const create = createToDo(el);
     listsNode.append(create);
   });
   hiddenBtn();
+  localStorage.setItem('toDo-lists', JSON.stringify(toDo));
 }
+renderToDo(toDo);
 // Функция изменения Done
 function checkboxChecked(id) {
   const check = toDo.find((el) => el.id == id);
   check.done = !check.done;
-  console.log(toDo);
 }
-btnDelNode.addEventListener("click", deleteFinish);
-// Функция удаления звершенных
+btnDelNode.addEventListener('click', deleteFinish);
+// Функция удаления завершенных
 function deleteFinish() {
   toDo = toDo.filter((el) => !el.done);
   renderToDo(toDo);
